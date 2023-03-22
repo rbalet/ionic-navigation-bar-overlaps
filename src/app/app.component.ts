8fbb9a5c38a659faf756c7d0f33f2c0006ceabf5
Raphael Balet <raphael@megaphone.info>
2023-03-22 06:32:18 +0100

    add - Code reproduction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Capacitor } from '@capacitor/core';
+import { SplashScreen } from '@capacitor/splash-screen';
+import { StatusBar } from '@capacitor/status-bar';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,24 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    this._init();
+  }
+
+  private async _init() {
+    try {
+      await SplashScreen.hide();
+    } catch (error) {
+      console.info('SplashScreen not supported', error);
+    }
+
+    this._setStatusBar();
+  }
+
+  private _setStatusBar() {
+    if (Capacitor.getPlatform() === 'android') {
+      StatusBar.setOverlaysWebView({ overlay: true });
+      StatusBar.setBackgroundColor({ color: '#00000000' });
+    }
+  }
 }
